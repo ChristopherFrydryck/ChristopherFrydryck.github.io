eac53dbc8a7e6a1256274c3af5f5720d17b771f3
ChristopherFrydryck <cfrydryck@gmail.com>
2018-08-19 21:18:12 -0400

    Update 4.0.1
    Major Design Update

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,19 +1,4 @@
 
-	$(function(){
-		$(".welcome-one").delay(500).fadeIn(1000);
-		$(".getStarted").delay(1500).fadeIn(1000);
-		$(".indexpara").delay(2500).fadeIn(1000);
-    $("#bkgImg").delay(2000).fadeIn(1000);
-    $("#bkgImgBottom").delay(2600).show();
-    $("#portfolioDiv").delay(2600).fadeIn(1000);
-    $("#contactDiv").delay(2600).fadeIn(1000);
-    $(".arrowDownContact").delay(2600).fadeIn(1000);
-    $("#bottomDiv").delay(2600).fadeIn(1000);
-
-  
-
-	});
-
 	$(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -62,6 +47,8 @@ $(document).ready(function() {
    if (w > 890) {
      $('nav').css('display', 'block');
   }
+
+
   }
 
   
@@ -70,8 +57,7 @@ $(document).ready(function() {
     if (showing == false) {
       $('nav').css('visibility', 'visible');
       showing = !showing;
-      $('nav').delay(300).slideDown(300);
-      $('#appearBox').fadeOut(300);
+      $('nav').slideDown(300);
       this.classList.toggle('change');
       //alert(showing);
 
@@ -92,17 +78,6 @@ $(document).ready(function() {
     
   });
 
-  $('.name').click(function(){
-      if (w <= 890) {
-      $('nav').slideUp(300);
-      showing = false;
-      $('#appearBox').delay(300).fadeToggle(300);
-      $('.mobile-toggle').removeClass('change');
-    }else{
-      $('#appearBox').delay(300).fadeToggle(300);
-    }
-
-  });
 
 
 
@@ -138,7 +113,7 @@ $(document).scroll(function() {
   }if (y >= portfolio.top - 120){
     $('.m-link').removeClass('active');
     $('.portfolio').addClass('active')
-  }if (y >= contact.top - 120){
+  }if (y >= contact.top - 120 || $(window).scrollTop() + $(window).height() == $(document).height()){
     $('.m-link').removeClass('active');
     $('.contact').addClass('active')
   }
@@ -146,35 +121,7 @@ $(document).scroll(function() {
 
   });
 
-  $('.portSub').click(function(){
-   
-    if($(this).hasClass('portValet') == true){
-      $('.portSub').removeClass('portActive');
-      $('.portValet').addClass('portActive');
-      $('.portInfo').html('Valet is an app design I am working on that is a parking solutions app for the public.  From early stages to logo design, Valet was a barebones challenge to redesign what never existed in the first place.  The design is an attempt at minimalist and flat design to maximize usability without providing a three dimensional hierarchy.');
-      $('.container').fadeOut();
-      $('.containerValet').delay(250).fadeIn();
-    }if($(this).hasClass('portLiveTracking') == true){
-      $('.portSub').removeClass('portActive');
-      $('.portLiveTracking').addClass('portActive');
-      $('.portInfo').html('Working with Freedom Transit in Washington, Pennsylvania to design and develop a live tracking web app was a challenge myself and my teammates were willing to step up to.  Using Google Firebase, Java, Javascript, HTML, CSS and JQuery to match their current website aesthetic, we accomplished and successfully completed a responsive web app that works for most devices, regardless of their browser or operating system.');
-      $('.container').fadeOut();
-      $('.containerLiveTracking').delay(250).fadeIn();
-    }if($(this).hasClass('portHumanIntrusion') == true){
-      $('.portSub').removeClass('portActive');
-      $('.portHumanIntrusion').addClass('portActive');
-      $('.portInfo').html('When I traveled out to the southwestern United States, I never expected to experience what I witnessed.  Scolding heat, flat plains and cacti everywhere is the stereotypical trip out west; what I really witnessed was a still beautiful, but well-established human sanctuary.  Water is soon becoming a luxury and what was once natural is now habited where humans don’t belong.  These are the photographs of human interaction with nature, but where we define intrusion varies.');
-      $('.container').fadeOut();
-      $('.containerHumanIntrusion').delay(250).fadeIn();
-    }if($(this).hasClass('portMega') == true){
-      $('.portSub').removeClass('portActive');
-      $('.portMega').addClass('portActive');
-      $('.portInfo').html('As a case study and overview of a security project, I was assigned Mega.nz, the successor to the infamous MegaUpload that was shut down by the FBI in 2012.  Mega.nz is a similar file sharing network among private parties that allows end-to-end encryption using AES.  This pamphlet was a project I designed and decided to add to my portfolio to showcase print design.');
-      $('.container').fadeOut();
-      $('.containerMega').delay(250).fadeIn();
 
-    }
-  });
 
 });  
   
